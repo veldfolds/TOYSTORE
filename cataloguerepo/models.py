da1fbe58cd26c8779eadfea56bc4af541bd3dd1d
Kubokaadrian <Kubokaadrian@outlook.com>
2022-08-04 00:04:27 +0300

    updated toy entity

--- cataloguerepo/models.py
+++ cataloguerepo/models.py
@@ -1,5 +1,10 @@
 from flask_mongoengine import MongoEngine
 from mongoengine import *
 
+#represents a specific toy on the shelf
 class Toy(Document):
-    name = StringField()
+    name = StringField(),
+    price = DecimalField(),
+    image_url = StringField,
+    year = DateField(),
+    stock = IntField()
