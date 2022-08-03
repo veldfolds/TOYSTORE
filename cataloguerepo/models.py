43501ca0e696d314980ad07e633849f6c3e0f562
Kubokaadrian <Kubokaadrian@outlook.com>
2022-08-04 00:08:51 +0300

    added company field to toy entity

--- cataloguerepo/models.py
+++ cataloguerepo/models.py
@@ -1,10 +1,12 @@
 from flask_mongoengine import MongoEngine
 from mongoengine import *
 
+db = MongoEngine()
 #represents a specific toy on the shelf
 class Toy(Document):
-    name = StringField(),
-    price = DecimalField(),
-    image_url = StringField,
+    name = StringField(max_length=20, min_length=1),
+    company = StringField(),
+    price = DecimalField(min_value=1),
+    image_url = StringField(),
     year = DateField(),
     stock = IntField()
